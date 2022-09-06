19ae353f36e49c845a5cfeb8047ad853f95b3967
suno <0910.shj@gmail.com>
2022-09-06 22:36:58 +0900

    Lv1 plus

---
+++ Lv1/76501.js
@@ -0,0 +1,11 @@
+function solution(absolutes, signs) {
+  let answer = 0;
+  signs.forEach((bool, idx) => {
+    if (bool) answer += absolutes[idx];
+    else answer -= absolutes[idx];
+  })
+  return answer;
+}
+
+console.log(solution([4, 7, 12], [true, false, true])); // 9
+console.log(solution([1, 2, 3], [false, false, true])); // 0
