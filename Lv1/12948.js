b027fa5fb9ed7157d7f65423d8adf995278b0ecc
suno <0910.shj@gmail.com>
2022-09-07 23:50:27 +0900

    Lv1 array slice

---
+++ Lv1/12948.js
@@ -0,0 +1,11 @@
+function solution(phone_number) {
+  let answer = '';
+  for (let i = 0; i < phone_number.length - 4; i++) {
+    answer += '*';
+  }
+  answer += phone_number.slice(phone_number.length - 4)
+  return answer;
+}
+
+console.log(solution('01033334444'));
+console.log(solution('027778888'));
