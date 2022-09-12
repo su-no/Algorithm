03b9226ec042579a1c5412500a5712d0f32e9da2
suno <0910.shj@gmail.com>
2022-09-12 19:39:56 +0900

    Lv1 array sort

---
+++ Lv1/12982.js
@@ -0,0 +1,16 @@
+function solution(d, budget) {
+  let result = 0;
+  const sortedD = d.sort((a, b) => { return a - b });
+  for (let i = 0; i < sortedD.length; i++) {
+    if (budget >= sortedD[i]) {
+      budget -= sortedD[i];
+      result += 1;
+    } else {
+      return result;
+    }
+  }
+  return result;
+}
+
+console.log(solution([1, 3, 2, 5, 4], 9)); // 3
+console.log(solution([2, 2, 3, 3], 10)); // 4
