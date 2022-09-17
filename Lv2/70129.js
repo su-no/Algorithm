2306e10810aeae2b8563faae219f2853b7bdc6ae
suno <0910.shj@gmail.com>
2022-09-17 22:50:41 +0900

    Lv2 binary to demical

---
+++ Lv2/70129.js
@@ -0,0 +1,27 @@
+function solution(s) {
+  let binaryNumber = s;
+  let countOfZero = 0;
+  let countOfConvert = 0;
+
+  // 변환된 이진수가 1이면 종료
+  while (binaryNumber !== '1') {
+    let zeroRemovedNumber = '';
+    for (let i = 0; i < binaryNumber.length; i++) {
+      if (binaryNumber[i] !== '0')
+        zeroRemovedNumber += binaryNumber[i];
+    }
+    // 제거된 0의 개수 더하기
+    countOfZero += binaryNumber.length - zeroRemovedNumber.length;
+
+    // 0이 제거된 수의 길이를 이진수로 변환
+    const lengthOfNumber = zeroRemovedNumber.length;
+    binaryNumber = lengthOfNumber.toString(2);
+    countOfConvert += 1;
+  }
+
+  return [countOfConvert, countOfZero];
+}
+
+console.log(solution("110010101001"));
+console.log(solution("01110"));
+console.log(solution("1111111"));
