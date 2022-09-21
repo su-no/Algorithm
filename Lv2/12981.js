4eac3d38a8ee5c6d4e099be55d76eaab7c0a3e1c
suno <0910.shj@gmail.com>
2022-09-22 00:28:47 +0900

    Lv2 english end talk

--- Lv2/12981.js
+++ Lv2/12981.js
@@ -1,18 +1,19 @@
 function solution(n, words) {
-  const usedWords = [];
-  for (let i = 0; i < words.length; i++) {
-    // 1) 이미 말한 단어
-    // 2) 끝말잇기 성립 안함
-    console.log(words.slice(-1))
-    //         else if (usedWords.includes(words[i])) {
-    //             usedWords.push(words[i]);
-    //         } else {
-
-    //         }
+  const usedWords = [words[0]];
+  let turn = 1;
+  for (let i = 1; i < words.length; i++) {
+    if (i % n === 0) turn++
+    // 1) 끝말잇기 성립하지 않는 경우
+    if (words[i - 1].slice(-1) !== words[i].slice(0, 1)) {
+      return [i % n + 1, turn];
+      // 2) 이미 사용한 단어일 경우
+    } else if (usedWords.includes(words[i])) {
+      return [i % n + 1, turn];
+    } else {
+      usedWords.push(words[i]);
+    }
   }
-  console.log(usedWords);
-
-  return;
+  return [0, 0];
 }
 
 console.log(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]));
