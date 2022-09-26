ca7340c1c83a941f2a535ae58a046b2677bd6a23
suno <0910.shj@gmail.com>
2022-09-26 15:38:27 +0900

    주석 추가

--- Lv2/17680.js
+++ Lv2/17680.js
@@ -2,16 +2,18 @@ function solution(cacheSize, cities) {
   // 캐시 사이즈가 0이면, 모두 cache miss.
   if (cacheSize === 0) return cities.length * 5;
 
-  let time = 0;
-  const queue = [];
+  // 대소문자 구분 없이 처리하기 위해, 모두 대문자로 바꾼다.
   const citiesUpperCase = cities.map((city) => {
     return city.toUpperCase();
   });
+  let time = 0;
+  const queue = [];
 
   for (let i = 0; i < citiesUpperCase.length; i++) {
     const searchCity = citiesUpperCase[i];
     const searchIndex = queue.indexOf(searchCity);
 
+    // cache miss
     if (searchIndex === -1) {
       if (queue.length >= cacheSize) {
         queue.shift();
@@ -19,6 +21,7 @@ function solution(cacheSize, cities) {
       queue.push(citiesUpperCase[i]);
       time += 5;
     } else {
+      // cache hit
       if (searchIndex !== cacheSize - 1) {
         queue.splice(searchIndex, 1);
         queue.push(searchCity);
