2d27c90566522babc911abd72cffcb739a2919fe
suno <0910.shj@gmail.com>
2022-09-26 15:10:51 +0900

    Refactoring

--- Lv2/12909.js
+++ Lv2/12909.js
@@ -1,15 +1,16 @@
 function solution(s) {
   const queue = [];
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === ')' && queue.at(-1) === '(') {
+    if (s[i] === ")" && queue.at(-1) === "(") {
       queue.pop();
     } else {
       queue.push(s[i]);
     }
   }
 
-  if (queue.length === 0) return true;
-  else return false;
+  return queue.length === 0;
+  // if (queue.length === 0) return true;
+  // else return false;
 }
 
 console.log(solution("()()")); // true
