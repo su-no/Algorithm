dae04ad96facc10e9f77ddbd46ad5f992a821714
suno <0910.shj@gmail.com>
2022-09-26 15:11:25 +0900

    Refactoring

--- Lv2/12953.js
+++ Lv2/12953.js
@@ -9,7 +9,7 @@ function solution(arr) {
 function getLCM(num1, num2) {
   let lcm = 1;
   while (true) {
-    if (lcm % num1 == 0 && lcm % num2 == 0) {
+    if (lcm % num1 === 0 && lcm % num2 === 0) {
       break;
     }
     lcm++;
