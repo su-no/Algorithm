9566eb5d79ad98c3a6d00d883339f4b61539c23a
suno <0910.shj@gmail.com>
2022-09-27 17:33:23 +0900

    Lv2 matrix multiplication

---
+++ Lv2/12949.js
@@ -0,0 +1,48 @@
+function solution(arr1, arr2) {
+  const answer = [];
+
+  for (let c = 0; c < arr1.length; c++) {
+    const row = [];
+
+    for (let r = 0; r < arr2[0].length; r++) {
+      let sum = 0;
+
+      for (let i = 0; i < arr1[0].length; i++) {
+        sum += arr1[c][i] * arr2[i][r];
+      }
+
+      row.push(sum);
+    }
+    answer.push(row);
+  }
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [1, 4],
+      [3, 2],
+      [4, 1],
+    ],
+    [
+      [3, 3],
+      [3, 3],
+    ]
+  )
+);
+
+console.log(
+  solution(
+    [
+      [2, 3, 2],
+      [4, 2, 4],
+      [3, 1, 4],
+    ],
+    [
+      [5, 4, 3],
+      [2, 4, 1],
+      [3, 1, 1],
+    ]
+  )
+);
