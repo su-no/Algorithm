200849775ac68616bed5b5c0674a5b7f451be74f
suno <0910.shj@gmail.com>
2022-10-07 23:49:05 +0900

    Lv2 주차 요금 계산

---
+++ Lv2/92341.js
@@ -0,0 +1,74 @@
+function solution(fees, records) {
+  // console.log('기본 시간, 기본 요금, 단위 시간, 단위 요금');
+  // console.log(fees);
+  // console.log('시각(시:분), 차량 번호, 내역');
+  // console.log(records);
+
+  const queue = {};
+  const cumulativeTime = {};
+
+  // 1. 누적 주차 시간을 구한다.
+  records.forEach((record) => {
+    const [time, number, state] = record.split(' ');
+    // console.log(time, number, state);
+    if (state === 'IN') {
+      queue[number] = time;
+    } else {
+      const outTime = time;
+      const inTime = queue[number];
+
+      // 1) arrivalTime과 출차 시간을 빼서 몇분인지 계산한다.
+      const netTime =
+        parseInt(60 * outTime.split(':')[0]) + parseInt(outTime.split(':')[1])
+        - parseInt(60 * inTime.split(':')[0]) - parseInt(inTime.split(':')[1]);
+
+      delete queue[number];
+
+      // 2)
+      // 두번째 들어온 경우
+      if (number in cumulativeTime) cumulativeTime[number] += netTime;
+      // 처음 들어온 경우
+      else cumulativeTime[number] = netTime;
+    }
+  });
+
+  console.log(cumulativeTime);
+  console.log(queue);
+  // 2. 기본 요금을 계산한다.
+  if (queue) {
+    for (const number in queue) {
+      const inTime = queue[number];
+      const netTime = 23 * 60 + 59
+        - parseInt(60 * inTime.split(':')[0]) -
+        parseInt(inTime.split(':')[1]);
+
+      if (number in cumulativeTime) cumulativeTime[number] += netTime;
+      else cumulativeTime[number] = netTime;
+    }
+  }
+  console.log(cumulativeTime);
+
+  return;
+}
+
+console.log(solution([180, 5000, 10, 600], [
+  '05:34 5961 IN',
+  '06:00 0000 IN',
+  '06:34 0000 OUT',
+  '07:59 5961 OUT',
+  '07:59 0148 IN',
+  '18:59 0000 IN',
+  '19:09 0148 OUT',
+  '22:59 5961 IN',
+  '23:00 5961 OUT'], [14600, 34400, 5000]
+));
+// console.log(solution([120, 0, 60, 591], [
+//   '16:00 3961 IN',
+//   '16:00 0202 IN',
+//   '18:00 3961 OUT',
+//   '18:00 0202 OUT',
+//   '23:58 3961 IN'], [0, 591]
+// ));
+// console.log(solution([1, 461, 1, 10],
+//   ['00:00 1234 IN'],
+//   [14841]));
