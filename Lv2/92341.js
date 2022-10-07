221528ae821c80836e3e35dac7005ae7a6548aca
suno <0910.shj@gmail.com>
2022-10-08 00:05:22 +0900

    Lv2 주차 요금 계산

--- Lv2/92341.js
+++ Lv2/92341.js
@@ -1,16 +1,10 @@
 function solution(fees, records) {
-  // console.log('기본 시간, 기본 요금, 단위 시간, 단위 요금');
-  // console.log(fees);
-  // console.log('시각(시:분), 차량 번호, 내역');
-  // console.log(records);
-
   const queue = {};
   const cumulativeTime = {};
 
   // 1. 누적 주차 시간을 구한다.
   records.forEach((record) => {
     const [time, number, state] = record.split(' ');
-    // console.log(time, number, state);
     if (state === 'IN') {
       queue[number] = time;
     } else {
@@ -34,7 +28,8 @@ function solution(fees, records) {
 
   console.log(cumulativeTime);
   console.log(queue);
-  // 2. 기본 요금을 계산한다.
+
+  // 2. 출차하지 않은 차량에 대해 23:59까지의 요금을 부과한다.
   if (queue) {
     for (const number in queue) {
       const inTime = queue[number];
@@ -48,27 +43,38 @@ function solution(fees, records) {
   }
   console.log(cumulativeTime);
 
-  return;
+  // 3. 요금을 계산하고, 차량 번호가 작은 순서대로 배열에 담는다.
+  const answer = [];
+  Object.keys(cumulativeTime).sort((a, b) => a - b).forEach(number => {
+    const time = cumulativeTime[number];
+    // 기본 시간, 기본 요금, 단위 시간, 단위 요금
+    if (time <= fees[0]) {
+      answer.push(fees[1]);
+    } else {
+      answer.push(fees[1] + Math.ceil((time - fees[0]) / fees[2]) * fees[3]);
+    }
+  });
+  return answer;
 }
 
-console.log(solution([180, 5000, 10, 600], [
-  '05:34 5961 IN',
-  '06:00 0000 IN',
-  '06:34 0000 OUT',
-  '07:59 5961 OUT',
-  '07:59 0148 IN',
-  '18:59 0000 IN',
-  '19:09 0148 OUT',
-  '22:59 5961 IN',
-  '23:00 5961 OUT'], [14600, 34400, 5000]
-));
-// console.log(solution([120, 0, 60, 591], [
-//   '16:00 3961 IN',
-//   '16:00 0202 IN',
-//   '18:00 3961 OUT',
-//   '18:00 0202 OUT',
-//   '23:58 3961 IN'], [0, 591]
+// console.log(solution([180, 5000, 10, 600], [
+//   '05:34 5961 IN',
+//   '06:00 0000 IN',
+//   '06:34 0000 OUT',
+//   '07:59 5961 OUT',
+//   '07:59 0148 IN',
+//   '18:59 0000 IN',
+//   '19:09 0148 OUT',
+//   '22:59 5961 IN',
+//   '23:00 5961 OUT'], [14600, 34400, 5000]
 // ));
+console.log(solution([120, 0, 60, 591], [
+  '16:00 3961 IN',
+  '16:00 0202 IN',
+  '18:00 3961 OUT',
+  '18:00 0202 OUT',
+  '23:58 3961 IN'], [0, 591]
+));
 // console.log(solution([1, 461, 1, 10],
 //   ['00:00 1234 IN'],
 //   [14841]));
