e559c6421adfa102e759b86bf1495b5d3a0b0d07
su-no <0910.shj@gmail.com>
2022-11-13 23:04:52 +0900

    Lv0 코딩테스트 입문 > 인덱스 바꾸기

--- Lv0/120895.js
+++ Lv0/120895.js
@@ -1,4 +1,5 @@
 function solution(my_string, num1, num2) {
+  // 반복문 사용, 해당 인덱스에서 인덱스 바꾸기
   let result = '';
   for (let i = 0; i < my_string.length; i++) {
     if (i === num1) {
@@ -12,5 +13,14 @@ function solution(my_string, num1, num2) {
   return result;
 }
 
-console.log(solution('hello', 1, 2)); // 'hlelo'
-console.log(solution('I love you', 3, 6)); // 'I l veoyou'
+function solution2(my_string, num1, num2) {
+  // 문자열을 배열로 만들어서, 해당 인덱스 주소에 할당값을 바꾸기
+  const str_array = my_string.split('');
+  [str_array[num1], str_array[num2]] = [str_array[num2], str_array[num1]];
+  return str_array.join('');
+}
+
+// console.log(solution('hello', 1, 2)); // 'hlelo'
+// console.log(solution('I love you', 3, 6)); // 'I l veoyou'
+console.log(solution2('hello', 1, 2)); // 'hlelo'
+console.log(solution2('I love you', 3, 6)); // 'I l veoyou'
