500332eed3aeedaeea89de86ea88b28de3bc6fc8
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 15:23:03 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -10,6 +10,9 @@ function solution(numbers) {
                   'eight': 8,
                   'nine': 9
                  }
+    // 1) 해당하는 문자를 배열로 만들어서 map 함수로 변환
     // return Number(numbers.match(/zero|one|two|three|four|five|six|seven|eight|nine/g).map(num => dict[num]).join(''));
+    
+    // 2) 해당하는 문자를 바로 숫자로 치환 => 더 빠름.
     return Number(numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, num => dict[num]));
 }
