fa6823d5a104d40ba4b3019a4cb8026b0ed87934
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 10:22:33 +0900

    [level 0] Title: 369게임, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,6 +1,9 @@
 function solution(order) {
     // return [...order.toString()].filter(num => (/[369]/).test(num)).length;
+    // 문자열이 각각 369에 포함되는지 확인 -> 결과 배열 길이 반환
+    
     return [...order.toString().matchAll(/[369]/g)].length;
+    // 문자열 전체에서 369에 포함되는 글자만 배열 생성 -> 길이 반환
 }
 
 // Number(num) % 3 === 0 // 0도 포함됨.
--- 프로그래머스/lv0/120891. 369게임/README.md
+++ 프로그래머스/lv0/120891. 369게임/README.md
@@ -1,6 +1,6 @@
 # [level 0] 369게임 - 120891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120891#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120891) 
 
 ### 성능 요약
 
