4de3604181749c37cc4e81b22dd64503ae2c6b51
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 10:39:39 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -2,3 +2,6 @@ function solution(before, after) {
     // return JSON.stringify([...before].sort()) === JSON.stringify([...after].sort()) ? 1 : 0;
     return [...before].sort().join('') === [...after].sort().join('') ? 1 : 0;
 }
+
+// before, after 문자열을 배열로 변환 -> 배열 문자열 오름차순 정렬 => 문자열로 변환 => 같은지 비교
+// 배열 -> 문자열 변환하는 방법 1) JSON.stringify(), 2) String.join('')
