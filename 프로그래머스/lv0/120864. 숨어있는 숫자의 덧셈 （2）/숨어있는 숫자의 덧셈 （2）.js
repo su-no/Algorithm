a7f535972e6d27aa6be44b9e74b0ee028f56d79d
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 15:14:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 3.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,4 +1,9 @@
 function solution(my_string) {
     const numbers = my_string.match(/\d+/g);
+    console.log(numbers);
     return numbers ? numbers.reduce((sum, cur) => sum + Number(cur), 0) : 0;
 }
+
+// 1. my_string에서 자연수를 찾아 배열로 반환한다. -> 연속된 숫자는 하나의 숫자로 간주한다. (/\d+/)
+// 2. 찾은 배열이 null일 경우 (자연수가 존재하지 않으면) 0을 반환
+// 3. 배열이 null이 아닐 경우 (자연수가 존재하면), 합계를 반환
