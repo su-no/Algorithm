125cc2accbcf18ebfb2db24e97b6ea8304a09282
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 10:51:24 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,4 +1,9 @@
 function solution(my_string) {
+    // 1. my_string 문자열을 배열로 변환한다.
+    // 2. 문자의 인덱스가 arr.indexOf(char) 과 같은 값인 문자를 반환한다. (indexOf는 일치하는 첫번째 값을 반환함)
+    // 3. 결과 배열을 문자열로 변환한다..
     // return [...my_string].filter((char, idx, arr) => arr.indexOf(char) === idx).join('');
+    
+    // 1. my_string을 Set으로 변환하고 (중복 제거), Set을 배열로 변환한 뒤 다시 문자열로 변환, 반환한다.
     return [...new Set(my_string)].join('');
 }
