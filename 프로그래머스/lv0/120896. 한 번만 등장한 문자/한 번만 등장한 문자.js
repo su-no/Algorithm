06f85142995483b2e3baac01b8a1e23938395305
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 14:41:49 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -2,3 +2,7 @@ function solution(s) {
     // return [...s].filter(char => s.split(char).length === 2).sort().join('');
     return [...s].filter(char => s.match(new RegExp(char, 'g')).length === 1).sort().join('');
 }
+
+// 시간
+// split : 0.40 ms
+// RegExp : 0.18 ms => 더 빠르다.
