c938ca49ac72970049a951e1f60ce64f351cafaa
Hyojin Sung <88768022+su-no@users.noreply.github.com>
2022-11-14 14:27:29 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -4,3 +4,8 @@ function solution(array, n) {
     const indexOfMin = relativeArray.indexOf(Math.min(...relativeArray));
     return sortedArray[indexOfMin];
 }
+
+// 1. 배열 숫자 오름차순 정렬
+// 2. 각 숫자에서 n만큼 빼고 절댓값 취하기
+// 3. 절댓값이 가장 작은 인덱스를 구하기 (여러개면 가장 앞에 있는 숫자 -> 더 작은 수)
+// 4. 원본 오름차순 정렬된 배열에서 해당 인덱스 숫자 리턴
