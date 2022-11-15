e1f042ba5bdf97f5ca9cb6cf062d2d782547e5a0
Hyojin <88768022+su-no@users.noreply.github.com>
2022-11-15 12:46:47 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,17 +1,19 @@
 function solution(s) {
-    // const numbers = s.split(/\d+ Z /g).join('');
-    // return numbers.split(' ').reduce((sum, cur) => sum + Number(cur), 0);
-    
     const array = s.split(' ');
     
+    // array에서 Z가 없어질 때까지
     while (array.includes('Z')) {
+        // 첫번째 Z의 인덱스를 구한다.
         const idx = array.indexOf('Z');
+        // 만약 Z 인덱스가 0이라면, Z 하나만 지움
         if (idx === 0) {
             array.shift();
             continue;
         }
+        // Z 인덱스가 0이 아니면, 앞 원소와 Z 두개를 지움
         array.splice(idx-1, 2);
     }
     
+    // 남은 원소들의 합을 리턴
     return array.reduce((sum, cur) => sum + Number(cur), 0);
 }
