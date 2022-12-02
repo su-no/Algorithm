ac2e278c78a8ce75a486cbc24d9a6247db921fa3
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-02 22:44:07 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,11 +1,9 @@
 function solution(id_pw, db) {
-    const [myId, myPw] = id_pw;
-    for (let i=0; i<db.length; i++) {
-        const data = db[i];
-        const [id, pw] = [data[0], data[1]];
-        if (id === myId) {
-            return pw === myPw ? 'login' : 'wrong pw';
-        }   
-    };
+    const [id, pw] = id_pw;
+    for (const user of db) {
+        if (user[0] === id) {
+            return user[1] === pw ? 'login' : 'wrong pw';
+        }
+    }
     return 'fail';
 }
