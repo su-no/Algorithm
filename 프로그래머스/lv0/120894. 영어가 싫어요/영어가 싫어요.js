e8dfc74229f45dc214f88ed590e21912b99d567e
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-05 16:27:11 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -13,10 +13,13 @@ function solution(numbers) {
         nine: 9,
     }
     
-    // 문자열 찾아서 배열로 나누기
-    const arr = numbers.match(/zero|one|two|three|four|five|six|seven|eight|nine/g);
-    // 각 문자열을 숫자로 치환, join으로 문자열로 이어서, number 타입으로 변환
-    const result = Number(arr.map(str => dict[str]).join(''));
+    // // 문자열 찾아서 배열로 나누기
+    // const arr = numbers.match(/zero|one|two|three|four|five|six|seven|eight|nine/g);
+    // // 각 문자열을 숫자로 치환, join으로 문자열로 이어서, number 타입으로 변환
+    // const result = Number(arr.map(str => dict[str]).join(''));
+    // return result;
     
-    return result;
+    // 더 간단한 방법. replace를 정규표현식으로 사용
+    const replaced = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, num => dict[num]);
+    return Number(replaced);
 }
