0d5e3d288544edfbcf4fb4b1be09b23e68945529
Hyojin <88768022+su-no@users.noreply.github.com>
2022-12-05 16:16:44 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,27 +1,26 @@
 function solution(keyinput, board) {
-    // 방향의 dx, dy를 obj로 저장
-    const direction = {
-        'left': [-1, 0],
-        'right': [1, 0],
-        'up': [0, 1],
-        'down': [0, -1]
-    };
+  // 초기 위치
+  let [x, y] = [0, 0];
 
-    // 초기 위치
-    let [x, y] = [0, 0];
-    // 최대 위치의 절댓값
-    const [maxX, maxY] = [(board[0]-1)/2, (board[1]-1)/2];
-    
-    keyinput.forEach(input => {
-        // 최대 위치에 있는 경우 입력 무시
-        if (input === 'left' && x === -maxX) return;
-        if (input === 'right' && x === maxX) return;
-        if (input === 'up' && y === maxY) return;
-        if (input === 'down' && y === -maxY) return;
-        
-        const [dx, dy] = direction[input];
-        x += dx;
-        y += dy;
-    })
-    return [x, y];
+  // X, Y의 절대값 최대
+  const maxX = (board[0]-1)/2;
+  const maxY = (board[1]-1)/2;
+
+  for (const key of keyinput) {
+    switch (key) {
+      case 'up':
+        y = Math.min(maxY, y+1);
+        break;
+      case 'down':
+        y = Math.max(-maxY, y-1);
+        break;
+      case 'left':
+        x = Math.max(-maxX, x-1);
+        break;
+      case 'right': 
+        x = Math.min(maxX, x+1);
+        break;
+    }
+  }
+  return [x, y];
 }
