0b4f1253433de000f8504cf8ad8bf070d339a383
Hyojin <88768022+su-no@users.noreply.github.com>
2023-02-06 00:08:29 +0900

    [level 2] Title: 올바른 괄호, Time: 2.54 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,8 +1,10 @@
 function solution(s){
     let cnt = 0;
+    
     for (let i=0; i<s.length; i++) {
         // (이면 +1, )이면 -1
-        s[i] === '(' ? cnt += 1 : cnt -= 1;
+        cnt += (s[i] === '(' ? 1 : -1);
+        
         // cnt가 마이너스가 되면 )가 더 많다는 뜻이므로 실패.
         if (cnt < 0) return false;
     }
