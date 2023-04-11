237261606ed24b678f84fee24b8805106d498091
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-11 19:40:10 +0900

    [level 3] Title: 베스트앨범, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,12 +1,15 @@
 function solution(genres, plays) {
+    // 1. 장르별 총 재생 횟수를 담는 객체를 생성한다.
     const playsByGenre = {};
     
     for (let i=0; i<genres.length; i++) {
         playsByGenre[genres[i]] = (playsByGenre[genres[i]] ?? 0) + plays[i];        
     }
     
+    // 2. 곡 정보를 객체로 담고 있는 배열을 생성한다.
     const dic = genres.map((genre, index) => ({index, genre, play: plays[index]}));
     
+    // 3. 2번의 배열을 장르, 재생 횟수, index 순으로 정렬한다.
     dic.sort((a, b) => {
         if (a.genre !== b.genre) return playsByGenre[b.genre] - playsByGenre[a.genre];
         if (a.play !== b.play) return b.play - a.play;
@@ -15,6 +18,7 @@ function solution(genres, plays) {
     
     const tempDic = {};
     
+    // 4. 같은 장르에서 2개까지 필터링해서 인덱스를 반환한다.
     const result = dic.filter(song => {
         if (tempDic[song.genre] >= 2) return false;
         tempDic[song.genre] = (tempDic[song.genre] ?? 0) + 1;
@@ -23,6 +27,3 @@ function solution(genres, plays) {
 
     return result;
 }
-
-// 1. 장르별 합계 구해서 상위 2개
-// 2. genres, plays 정렬
