b3190f2531614d2d66f2d2ae442ac2aa85108751
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-11 22:14:04 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,21 @@
+function solution(bridge_length, weight, truck_weights) {
+    const onBridge = [];
+    
+    let totalWeight = 0;
+    let time = 1;
+    while (truck_weights.length > 0) {
+        if (onBridge.length > 0 && onBridge[0].endTime <= time) {
+            totalWeight -= onBridge[0].weight;
+            onBridge.shift();
+        }
+        
+        if (totalWeight + truck_weights[0] <= weight) {
+            totalWeight += truck_weights[0];
+            onBridge.push({weight: truck_weights.shift(), endTime: time + bridge_length});
+        }
+        
+        time++;
+    }
+    
+    return onBridge.at(-1).endTime;
+}
