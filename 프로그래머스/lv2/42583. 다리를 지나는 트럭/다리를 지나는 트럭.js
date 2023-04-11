ab9636878c7c1ee57e06b2064ea2e38898d4c95b
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-11 22:20:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,24 +1,33 @@
 function solution(bridge_length, weight, truck_weights) {
-    const onBridge = [];
+    // 다리를 지나고 있는 트럭
+    const queue = [];
     
+    // 다리 위 트럭의 무게
     let totalWeight = 0;
+    // 시간
     let time = 1;
+    
+    // 대기 트럭이 존재할 때까지 반복
     while (truck_weights.length > 0) {
-        if (onBridge.length > 0 && onBridge[0].endTime <= time) {
-            totalWeight -= onBridge[0].weight;
-            onBridge.shift();
+        // 다리 위에 트럭이 있고 첫번째 트럭이 나갈 시간이면, queue에서 제거하고 무게 제거
+        if (queue.length > 0 && queue[0].endTime <= time) {
+            totalWeight -= queue[0].weight;
+            queue.shift();
         }
         
+        // 트럭이 올라와도 되는 무게면 무게를 더해주고, queue에 추가 (무게와 나가는 시간)
         if (totalWeight + truck_weights[0] <= weight) {
             totalWeight += truck_weights[0];
-            onBridge.push({weight: truck_weights.shift(), endTime: time + bridge_length});
+            queue.push({weight: truck_weights.shift(), endTime: time + bridge_length});
         } else {
-            time = onBridge[0].endTime;
+            // 올라오지 못하는 무게이면 time 점프
+            time = queue[0].endTime;
             continue;
         }
         
         time++;
     }
     
-    return onBridge.at(-1).endTime;
+    // 대기 트럭이 없으면 종료 시간은 맨 마지막 트럭이 나가는 시간
+    return queue.at(-1).endTime;
 }
