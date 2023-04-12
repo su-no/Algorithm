e742e7083e0cd4675d79ec9786afd479217a2c31
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-12 21:28:38 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -7,7 +7,7 @@ function solution(citations) {
     // 인덱스+1보다 값이 크다면 hIndex 업데이트
     for (let i=0; i<sortedArray.length; i++ ) {
         if (i + 1 <= sortedArray[i]) {
-            hIndex = i + 1;
+            hIndex++;
         }
     }
     
