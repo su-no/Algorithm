8e598e606ddca6deb134e2ee9b2e223dabc8ef0a
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-13 00:37:21 +0900

    [level 1] Title: 모의고사, Time: 3.16 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,15 +1,15 @@
 function solution(answers) {
     let score = [0, 0, 0];
     
-    const answer1 = [1, 2, 3, 4, 5];
-    const answer2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    const answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const pattern1 = [1, 2, 3, 4, 5];
+    const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
     for (let i=0; i<answers.length; i++) {
         const answer = answers[i];    
-        if (answer === answer1[i % 5]) score[0]++;
-        if (answer === answer2[i % 8]) score[1]++;
-        if (answer === answer3[i % 10]) score[2]++;
+        if (answer === pattern1[i % 5]) score[0]++;
+        if (answer === pattern2[i % 8]) score[1]++;
+        if (answer === pattern3[i % 10]) score[2]++;
     }
     
     const maxScore = Math.max(...score);
