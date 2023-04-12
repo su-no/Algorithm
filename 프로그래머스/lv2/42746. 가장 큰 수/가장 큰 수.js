aca93bbf1b12f70fbab7711d193e2e4254b8dc20
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-12 21:14:35 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,7 +1,8 @@
 function solution(numbers) {
-    const result = numbers.sort((a, b) => {
-        return (b.toString() + a.toString()) - (a.toString() + b.toString());
-    }).join('');
+    const result = numbers
+                    .map(number => number.toString())
+                    .sort((a, b) => (b + a) - (a + b))
+                    .join('');
     
     return result[0] === '0' ? '0' : result;
 }
