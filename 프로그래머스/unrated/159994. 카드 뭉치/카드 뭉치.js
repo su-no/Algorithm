2e1300dd2bf7cb40f8854efa722d5c947e00df9e
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-16 16:25:03 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,24 +1,37 @@
 function solution(cards1, cards2, goal) {
-    // cards1부터 첫번째 원소 확인
-    // 맞으면 cards1, goal에서 shift하고 다음 요소 확인
-    // 틀리면 cards로 이동
-    
-    let isCards1 = true;
-    let switchingCount = 0;
-    
-    while (goal.length > 0 && switchingCount < 2) {
-        let cards = isCards1 ? cards1 : cards2;
-        if (cards[0] === goal[0]) {
-            cards.shift();
-            goal.shift();
-            switchingCount = 0;
-            continue;
+    for (const s of goal) {
+        if (cards1[0] === s) {
+            cards1.shift();
+        } else if (cards2[0] === s) {
+            cards2.shift();
         } else {
-            isCards1 = !isCards1;
-            switchingCount++;
-            continue;
+            return 'No';
         }
     }
-    
-    return goal.length === 0 ? 'Yes' : 'No';
+    return 'Yes';
 }
+
+// function solution(cards1, cards2, goal) {
+//     // cards1부터 첫번째 원소 확인
+//     // 맞으면 cards1, goal에서 shift하고 다음 요소 확인
+//     // 틀리면 cards로 이동
+    
+//     let isCards1 = true;
+//     let switchingCount = 0;
+    
+//     while (goal.length > 0 && switchingCount < 2) {
+//         let cards = isCards1 ? cards1 : cards2;
+//         if (cards[0] === goal[0]) {
+//             cards.shift();
+//             goal.shift();
+//             switchingCount = 0;
+//             continue;
+//         } else {
+//             isCards1 = !isCards1;
+//             switchingCount++;
+//             continue;
+//         }
+//     }
+    
+//     return goal.length === 0 ? 'Yes' : 'No';
+// }
