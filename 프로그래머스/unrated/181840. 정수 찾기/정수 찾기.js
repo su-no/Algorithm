04343c0b9b0d6feb4a04b303c7a03a6d7e04a3ba
Hyojin <88768022+su-no@users.noreply.github.com>
2023-04-26 16:26:29 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -1,3 +1,4 @@
 function solution(num_list, n) {
-    return num_list.some(num => num === n) ? 1 : 0;
+    // return num_list.some(num => num === n) ? 1 : 0;
+    return num_list.includes(n) ? 1 : 0;
 }
