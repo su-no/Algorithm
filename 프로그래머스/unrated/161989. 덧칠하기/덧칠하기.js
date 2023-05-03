1c11e3a585a042918468ce7e4a34110afe8a23f5
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-03 17:20:50 +0900

    [unrated] Title: 덧칠하기, Time: 10.35 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,14 +1,14 @@
 function solution(n, m, section) {
-    const wall = Array(n + 1).fill(true);
+    const painted = Array(n + 1).fill(true);
     for (const s of section) {
-        wall[s] = false;
+        painted[s] = false;
     }
     
     let cnt = 0;
     for (let i=1; i<n+1; i++) {
-        if (wall[i]) continue;
+        if (painted[i]) continue;
         for (let j=i; j<i+m; j++) {
-            wall[j] = true;
+            painted[j] = true;
         }
         cnt++;
     }
