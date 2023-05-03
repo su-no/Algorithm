61e5bbb06cf75643862bfd0c028c9862b763c706
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-03 16:16:50 +0900

    [unrated] Title: 옹알이 (2), Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,11 +2,11 @@ function solution(babbling) {
     let result = 0;
     for (const word of babbling) {
         // 연속된 발음이 있으면 못함
-        if (/ayaaya|yeye|woowoo|mama/.test(word)) {
+        if (/(aya|ye|woo|ma)\1+/.test(word)) {
             continue;
         }
         // 네가지 발음으로 조합 가능하면 가능
-        if (word.replaceAll(/aya|ye|woo|ma/g, '') === '') {
+        if (/^(aya|ye|woo|ma)+$/.test(word)) {
             result++;
         }
     }
