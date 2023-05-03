dac3fcf2766feced2a6af08a79f2a91078012e6a
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-03 17:53:31 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -6,21 +6,16 @@ function solution(X, Y) {
     Y.split('').forEach(num => arrayY[num] += 1);
     
     const common = arrayX.map((num, idx) => {
-        if (num <= arrayY[idx]) {
-            return num;
-        } else {
-            return arrayY[idx];
-        }
+        return Math.min(num, arrayY[idx])
     })
     
     // 짝꿍이 존재하지 않는 경우
-    if (common.every(num => num === 0)) {
+    if (/^0{10}$/.test(common.join(''))) {
         return '-1';
     }
     
     // 짝꿍이 0으로만 구성되어 있는 경우
-    if (common[0] !== 0 
-        && common.slice(1).join('') === '0'.repeat(9)) {
+    if (/^[1-9]0{9}$/.test(common.join(''))) {
         return '0';
     }
     
