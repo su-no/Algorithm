c756a8ab4200f584ee74c9969a3aff7726952164
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-06 14:12:38 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,8 +1,7 @@
 function solution(myString, pat) {
-    const replacedString = myString
-                            .replaceAll('B', 'T')
-                            .replaceAll('A', 'B')
-                            .replaceAll('T', 'A');
-    
+    const replacedString = [...myString].map(char => {
+        return char === 'A' ? 'B': 'A';
+    }).join('');
+
     return replacedString.includes(pat) ? 1 : 0;
 }
