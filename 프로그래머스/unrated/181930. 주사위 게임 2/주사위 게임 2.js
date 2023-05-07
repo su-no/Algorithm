d0dade33160b67691725fb793f0bdf6a533de183
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-08 02:13:53 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,9 @@
+function solution(a, b, c) {
+    if ((a === b) && (a === c) && (b === c)) {
+        return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+    } else if ((a !== b) && (a !== c) && (b !== c)) {
+        return a + b + c;
+    } else {
+        return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+    }
+}
