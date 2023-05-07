391bb4548a84f4b1a61991d03d9deaa09ea0dfe4
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-08 01:24:43 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,14 @@
+function solution(num_list) {
+    let evenSum = 0;
+    let oddSum = 0;
+    
+    for (let i=0; i<num_list.length; i++) {
+        if (i % 2 === 0) {
+            evenSum += num_list[i];
+        } else {
+            oddSum += num_list[i];
+        }
+    }
+    
+    return Math.max(evenSum, oddSum);
+}
