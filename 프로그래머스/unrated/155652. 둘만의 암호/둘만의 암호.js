65e223d8c3de6368204e92c5fea8840306220c42
Hyojin <88768022+su-no@users.noreply.github.com>
2023-05-10 17:40:27 +0900

    [unrated] Title: 둘만의 암호, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -1,4 +1,10 @@
+// s 문자열을 순회하면서
+// skip 문자를 건너뛴 index만큼 뒤의 문자 아스키코드를 구하고
+// 배열에 추가한다.
+// 배열을 문자열로 변환한다.
+
 function solution(s, skip, index) {
+    // skip 문자의 아스키 코드
     const skipAscii = [...skip].map((_, idx) => skip.charCodeAt(idx));
     const result = [];
     
@@ -6,14 +12,18 @@ function solution(s, skip, index) {
         let ascii = s.charCodeAt(i);
         let cnt = 0;
         
+        // index만큼 건너뛸 때까지 반복
         while (cnt < index) {
             ascii++;
+            // z를 넘어가면 a로 돌아간다.
             if (ascii > 122) {
                 ascii = 97;
             }
+            // skip 문자열에 포함될 경우 무시
             if (skipAscii.includes(ascii)) {
                 continue;
             }
+            // skip 문자열에 포함되지 않을 경우 index cnt 추가
             cnt++;
         }
         
